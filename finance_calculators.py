a689b37317aa319a8bac2d3cddd1ac4d99ef35c1
Namusale Chama <namusale@gmail.com>
2024-02-19 02:00:36 +0000

    added file for finance finance_calculators.py

---
+++ finance_calculators.py
@@ -0,0 +1,96 @@
+"""" The program works to calculate how much money an investor gets after depositing money for some period at a chosen interst rate. 
+      Also for those who wish to borrow, the expected monthly repayment amount is calculated based on property value, interest and 
+      duration of repayment. """
+
+
+import math
+
+# Displaying options and corresponding information for the user
+print('---------------------------------------------------------------------------------')
+print('                                                                                 ')
+print("investment - to calculate the amount of interest you'll earn on your investment")
+print("bond       - to calculate the amount you'll pay on your home loan\n")
+print('---------------------------------------------------------------------------------')
+
+# Inputing service request from the user
+print('                                                                                 ')
+user_input = input("Enter either 'investment' or 'bond' from the menu above to proceed:")
+
+# Lowercasing the user input
+user_input_lower = user_input.lower()
+#print(user_input_lower)
+
+
+# Checking if the user request is one of the available options
+if(not((user_input_lower == 'investment') or (user_input_lower== 'bond'))):
+    print(' There appears to be an error with your input. Please start again.')
+    
+# User data collection for investing individuals
+elif user_input_lower == 'investment':
+     
+    print('                                                                                 ')
+    print('                            INVESTMENT USER DATA                      ')
+    print('   ')
+    amount_inv = float(input('Please enter how much money you are depositing. '))
+    rate_inv = int(input('What is the interest rate you wish to use? '))
+    years_inv = int(input('How many years do you plan to invest? '))
+    interest = input('Do you want simple or compound interest? ')
+    print('')
+    print('')
+
+    # Calculating interest for investment- simple and compound
+    total_simple = amount_inv *(1+(rate_inv/100)*years_inv)
+
+    total_comp = amount_inv * math.pow((1+(rate_inv/100)),years_inv)
+
+    # Printing interest gained for investment with simple and compound interest
+
+    print('                            MONEY TO BE RECEIVED INVESTMENT                    ')
+    print('')
+    print('------------------------------------------------------------------------------')
+    if interest == 'simple':
+        print(f'The amount obtained after {years_inv} years and interest rate of {rate_inv}% is {round(total_simple,2)}')
+
+    elif interest == 'compound':
+        print(f'The amount obtained after {years_inv} years and interest rate of {rate_inv}% is {round(total_comp,2)}')
+
+    print('------------------------------------------------------------------------------')
+    print('')
+
+# User data collection for borrowing individuals
+elif user_input_lower == 'bond':
+    print('                                                                                 ')
+    
+    print('                                                                                 ')
+    print('                            BOND USER DATA                      ')
+    print('   ')
+    house_value = float(input("Please enter the value of your house. "))
+    rate_bond = int(input('What is your desired interest rate? '))
+    months = int(input('In how many months do you intend to replay the bond? '))
+    print('   ')
+    print('   ')
+
+    # Calculating bond repayment value
+     
+    rate_bond_updated = (rate_bond/100)/12
+
+    repayment = (rate_bond_updated * house_value)/(1 - (1 + rate_bond_updated)**(-months))
+
+    # Printing the expected monthly payment
+    print('                            MONTHLY REPAYMENT                     ')
+    print('')
+    print('-------------------------------------------------------------------------------------------------------------')
+    print(f"The amount of money to be repaid every month at a interest rate of {rate_bond}% is {round(repayment,2)}.")
+    print('--------------------------------------------------------------------------------------------------------------')
+    print('')
+
+# Add the advice
+    
+    if  user_input_lower== 'bond' and repayment > 0.25 * house_value:
+        print("")
+        print("")
+        print('----------------------------------------------------------------------------------------------------------')
+        print("The interest appears to be high. May we suggest that you lower the rate and/or extend period of payment")
+        print('----------------------------------------------------------------------------------------------------------')
+        print('')
+        
---
+++ protein_decode.py
@@ -0,0 +1,93 @@
+'''Ths program works to decode the codons present in the protein RNA. The stages involved are segmenting the
+protein in strings of length 3. Once the strings are identified as stops, sequencing ceases.The 3 lengthed strings are
+ looked up to a dictionary after which they are protein list is updated arccodingly. '''
+
+RNA = "AUGUUUUCUUAAAUG" # Protein sequence
+
+# Dictionary for looking up the codorns to their corresponding proteins(Extract)
+
+codons =  {
+            "AUG": "Methionine",
+            "UUU": "Phenylalanine" ,
+            "UUC": "Phenylalanine",
+            "UCU": "Serine",
+            "UUA": "Leucine ",
+            "UAA": "stop",
+            "UCU": "Serine",
+            "UCC": "Serine",
+            "UCA": "Serine",
+            "UCG": "Serine",
+            "UGU":"Cysteine",
+              "UGC":"Cysteine",
+               "UAU":"Tyrosine", 
+               "UAC":	"Tyrosine"
+            }
+
+#Lists to hold data
+seg = []
+protein_list =[]
+
+
+'''The decode function decodes the received codon (as an argument) by looking up the dictionary and updates
+   the protein list with corresponding proteins found in the sequence.'''
+
+def decode(codon):
+    for item in codons:
+        if (codon == item):
+            protein_list.append(codons[item])
+           
+
+'''The segment function  receives the sequence and segments it into lists of length 3 (codons). The stop cordon 
+is identified and codon segmentation stops. The function then calls on 
+the decode function that looks up the codons for the corresponding proteins.'''
+
+def segment(RNA):
+    seg_holder = []
+
+    for i in range(0,len(RNA)):
+       seg.append(RNA[i])
+       if (len(seg) == 3):
+           #print(seg)
+           new_cordon = str("".join(seg))
+           if new_cordon != "UAA":
+            #print(new_cordon)
+                seg.clear()
+                decode(new_cordon)
+            
+           else:
+                break
+           
+
+
+# The translator function prints the RNA sequence and its corresponding proteins by calling the segment function for detail
+           
+def translator():
+    segment(RNA)
+    print('')  
+    print('')  
+    print("===================================================================================================") 
+    print('')  
+    print('')  
+    print('')  
+    print("                            PROTEIN TRANSLATOR")
+    print('')
+    print('') 
+    print('====================================================================================================')
+    print('') 
+    print('') 
+    print(f"  RNA Sequence: {RNA} has  {' '.join(protein_list)} proteins ")
+    print('')  
+    print('')  
+    print('')    
+
+translator()
+
+
+
+
+    
+
+        
+            
+
+
